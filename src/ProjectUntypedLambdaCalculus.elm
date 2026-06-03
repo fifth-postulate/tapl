266ff0950de4afee1d8534721209b43a8d3bbb49
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2026-06-03 11:25:54 +0200

    Use same context for parsing, printing and evaluating

--- src/ProjectUntypedLambdaCalculus.elm
+++ src/ProjectUntypedLambdaCalculus.elm
@@ -2,13 +2,13 @@ module ProjectUntypedLambdaCalculus exposing (main)
 
 import General
 import GenericPage
-import UntypedLambdaCalculus as Expression exposing (default, empty)
+import UntypedLambdaCalculus as Expression exposing (default)
 
 
 main =
     GenericPage.create
         { title = "Untyped Lambda Calculus"
         , parse = Expression.parse default
-        , printer = Expression.toString empty
-        , evaluator = General.eval (Expression.step empty)
+        , printer = Expression.toString default
+        , evaluator = General.eval (Expression.step default)
         }
